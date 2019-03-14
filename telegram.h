3850d26936ba7516f9feb70efee14718cf9eca5c
root <root@alpha1.kiava.co>
2019-03-14 19:17:12 -0400

    adding files

---
+++ telegram.h
@@ -0,0 +1,33 @@
+/*
+    This file is part of telegram-cli.
+
+    Telegram-cli is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    Telegram-cli is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this telegram-cli.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright Vitaly Valtman 2013-2015
+*/
+
+#pragma once
+
+#ifndef PROG_NAME
+#define PROG_NAME "telegram-bot"
+#endif
+
+#define TELEGRAM_CLI_VERSION "TEQELRA"
+#define TELEGRAM_CLI_API_HASH "06625c2f1f9116fb0f6bbbd496619835"
+#define TELEGRAM_CLI_API_ID 664281
+#define TELEGRAM_CLI_DEVICE_MODEL "samsung"
+#define TELEGRAM_CLI_SYSTEM_VERSION "26"
+ 
+#define TELEGRAM_CLI_VERSION_STR \
+"Telegram-bot version " TELEGRAM_CLI_VERSION
